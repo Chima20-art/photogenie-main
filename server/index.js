e52bfb861c4e6860c0d75de06df4ad0b0383dccb
AvotraIsmael <105560504+IsmaelAvotra@users.noreply.github.com>
2023-04-14 16:35:33 +0000

    added necessary routes for user auth.

--- server/index.js
+++ server/index.js
@@ -1,35 +1,34 @@
 //import packages
-const express =require ('express');
-const bodyParser = require("body-parser");
-const mongoose= require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
-const dbURL =process.env.DB_URL;
+const dbURL = process.env.DB_URL;
 
 //Database connect
-mongoose.connect(dbURL).then(()=>{
-    console.log('Connection successful');
-}).catch(e=>{
-    console.log(e);
-});
+mongoose
+    .connect(dbURL)
+    .then(() => {
+        console.log('Connection successful');
+    })
+    .catch((e) => {
+        console.log(e);
+    });
 
 // import all files
-const authRouter= require("./routes/auth");
+const authRouter = require('./routes/auth');
 
 //Init
-const PORT=3000;
-const app =express();
-
-
-//middleware 
-app.use(express.json());
-app.use(authRouter);
-
+const PORT = 3000;
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//middleware
+app.use(express.json());
+app.use(authRouter);
 
-
-app.listen(PORT,"0.0.0.0", ()=>{
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Connect at port ${PORT}`);
 });
